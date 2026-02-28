d64863594f6e0fae35164d0f2995a904a50041d6
UdayKohai <Udaimathur3114@gmail.com>
2026-02-28 14:18:44 +0530

    Task2

--- Day-11/tests/task2.spec.ts
+++ Day-11/tests/task2.spec.ts
@@ -1,4 +1,13 @@
 import {test} from '@playwright/test'
 
 test("test2",async({page,browserName})=>{
+    await page.goto("https://www.flipkart.com/");
+    await page.locator('//span[@class="b3wTlE"]').click();
+    await page.locator('//input[@class="nw1UBF v1zwn25"]').first().fill("Phones");
+    await page.locator('(//button[@class="XFwMiH"])').first().click();
+    await page.locator('(//div[@class="ybaCDx"])').first().click();
+    let price = await page.locator('(//div[@class="hZ3P6w DeU9vF"])').nth(3).textContent();
+    console.log(price);
+    await page.screenshot({path:`screenshot/task2${browserName}.png`})
+    
 })
