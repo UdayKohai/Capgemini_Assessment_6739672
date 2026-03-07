13cd90f412eeef43ad6456ff3c81564e031c43b6
UdayKohai <Udaimathur3114@gmail.com>
2026-03-07 13:29:50 +0530

    Day 13 assessment task-2

---
+++ Day-13/tests/task-2.spec.ts
@@ -0,0 +1,17 @@
+import {test} from '@playwright/test';
+
+test("Task-1",async({page,browserName})=>{
+    // await page.goto('https://www.google.com/');
+    // await page.locator('//textarea[@class="gLFyf"]').fill('tokyo olympics');
+    // await page.locator('(//input[@class="gNO89b"])').first().click();
+    // await page.locator('//span[text()="Tokyo 2020 Summer Olympics - Athletes, Medals & Results"]').click();
+    await page.goto('https://www.olympics.com/en/olympic-games/tokyo-2020');
+    await page.locator('//button[@id="onetrust-accept-btn-handler"]').click();
+    await page.locator('//a[text()="All Athletes"]/child::button').click();
+    let medal = await page.locator('//h3[contains(text(),"ZHANG")]/ancestor::li/descendant::div[@title="Silver"]/descendant::span[@data-cy="ocs-text-module"]').textContent();
+    console.log("Silver Medal = ",medal);
+
+    await page.screenshot({path:`Screenshot/task2${browserName}.png`})
+    
+
+})
